d6a11cacd052274306c867621756e4138cd8fa95
zhangjunling4 <15637181917@163.com>
2017-12-07 16:24:02 +0800

    项目初始化

---
+++ app/src/main/java/com/seek/birthdayparty/BirthdayPartyApplication.java
@@ -0,0 +1,15 @@
+package com.seek.birthdayparty;
+
+import android.app.Application;
+
+/**
+ * Created by admin on 2017/12/7.
+ */
+public class BirthdayPartyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
